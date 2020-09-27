97d67992e4dfc80156d836cf58ffb61ba7695516
stefanog1973@gmail.com <Paolo2006!>
2020-09-27 10:30:41 +0200

    levati un po di warning

--- main.py
+++ main.py
@@ -53,15 +53,10 @@ with open('data.txt') as json_file:
 
 for p in data['people']:
     print('titolo da file di testo=' + p['name'])
-    #nome_titoli.append(p)
 
 data_inizio = '2019-9-14'
 data_fine = '2020-9-14'
 print('inizio caricamento')
-
-
-indice = 0
-#lunghezza = len(nome_titoli)
 
 init_notebook_mode(connected=True)
 cf.go_offline()
@@ -70,59 +65,54 @@ cf.go_offline()
 for p in data['people']:
     print('titolo da vettore=' + p['name'])
     try:
-         dataframe = web.get_data_yahoo(p['name'], data_inizio, data_fine, interval='d')
+        dataframe = web.get_data_yahoo(p['name'], data_inizio, data_fine, interval='d')
     except:
         print('titolo non trovato:' + p['name'])
         continue
-    print(p['name'],dataframe.shape)
-    print(p['name'] + 'last' ,dataframe.tail())
+    print(p['name'], dataframe.shape)
+    print(p['name'] + 'last', dataframe.tail())
     print('fine caricamento')
 
+    quotazioni = dataframe.values
+    lista_chiusura = quotazioni[:, 5]
+    lista_rendimenti = np.array([])
 
-    quotazioni=dataframe.values
-    lista_chiusura=quotazioni[:,5]
-    lista_rendimenti=np.array([])
-    i=0
-    lunghezza=lista_chiusura.size
-    indice += 1
+    lunghezza = lista_chiusura.size
 
-    quotaChiusura=dataframe['Close']
+    quotaChiusura = dataframe['Close']
     quotaChiusura.plot(title=p['name'] + ' quota')
-
-    #returns = dataframe['Close'].pct_change()
-    #((1 + returns).cumprod() - 1).plot(title= titolo + ' Cumulative Returns')
-    #dataframe.plot(title= titolo + 'Adj. Closing Price')
-    #dataframe.set_index('Date')['Adj'].plot()
-    #dataframe.plot(x='Date',y='Close')
+    # returns = dataframe['Close'].pct_change()
+    # ((1 + returns).cumprod() - 1).plot(title= titolo + ' Cumulative Returns')
+    # dataframe.plot(title= titolo + 'Adj. Closing Price')
+    # dataframe.set_index('Date')['Adj'].plot()
+    # dataframe.plot(x='Date',y='Close')
     plt.show()
     nomefilegrafico = p['name'] + '.jpg'
     plt.savefig(nomefilegrafico)
 
-#https://michaelsaruggia.com/data-visualization-plotly/
+# https://michaelsaruggia.com/data-visualization-plotly/
 
-#init_notebook_mode(connected=True)
-#cf.go_offline()
-#for titolo in enumerate(nome_titoli):
-#data.head();
-
-
+# init_notebook_mode(connected=True)
+# cf.go_offline()
+# for titolo in enumerate(nome_titoli):
+# data.head();
 
 # Prepare the data
-#x = np.linspace(0, 10, 100)
+# x = np.linspace(0, 10, 100)
 
 # Plot the data
-#plt.plot(x, x, label='linear')
+# plt.plot(x, x, label='linear')
 
 # Add a legend
-#plt.legend()
+# plt.legend()
 
 # Show the plot
-#plt.show()
-#data.iplot(x=mioViettorex,y=mioVettorey,mode='lines+markers')
+# plt.show()
+# data.iplot(x=mioViettorex,y=mioVettorey,mode='lines+markers')
 
 
 
-#while(i < lunghezza - 1):
+# while(i < lunghezza - 1):
 #    rendimento = np.log(lista_chiusura[i+1]/lista_chiusura[i])
 #    lista_rendimenti = np.append(lista_rendimenti, [rendimento])
 #    i = i + 1
